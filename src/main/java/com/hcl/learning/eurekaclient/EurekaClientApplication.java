1558f6017339056dcad266d4d6a429940d54f024
mukul <mukul.k@hcl.com>
2018-10-15 09:47:10 +0530

    first commit

---
+++ src/main/java/com/hcl/learning/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,23 @@
+package com.hcl.learning.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	@RequestMapping("/hello")
+    public String greeting() {
+        return "Hello Eureka Client Micro Service !";
+    }
+}
+
+
